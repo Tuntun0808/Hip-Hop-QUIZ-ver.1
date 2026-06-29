82723b823bfff6186d3ebbef0ed6de5d00cab4e0
Tuntun0808 <tuntun0864@gmail.com>
2026-06-29 23:51:23 +0900

    Add files via upload

---
+++ jhiphop.js
@@ -0,0 +1,198 @@
+let jQuiz = [
+
+{
+question:"RAPSTAR 2025の優勝者は誰？",
+
+answers:[
+"MASATO HAYASHI",
+"Pxrge Trxxxper",
+"Tee Shyne",
+"VERRY SMoL"
+],
+
+correct:1
+},
+
+
+{
+question:"ラッパーZORNの本名は？",
+
+answers:[
+"杉山 雄基",
+"武田 嘉穂",
+"川上 国彦",
+"岩瀬 雄哉"
+],
+
+correct:0
+},
+
+
+{
+question:"RHYMESTERのDJは誰？",
+
+answers:[
+"DJ KANJI",
+"DJ TATSUKI",
+"DJ OZMA",
+"DJ JIN"
+],
+
+correct:3
+},
+
+
+{
+question:"「なめだるま」の正しい表記はどれ？",
+
+answers:[
+"滑達磨",
+"舐達磨",
+"舐達摩",
+"舐達麻"
+],
+
+correct:3
+},
+
+
+{
+question:"NITRO MICROPHONE UNDERGROUNDの曲はどれ？",
+
+answers:[
+"ヤッホー",
+"オレはオレ",
+"たてめえん",
+"ちょっとだけバカ"
+],
+
+correct:2
+},
+
+
+{
+question:"ラッパ我リヤの名曲「ヤバスギルスキル」は、2026年現在Partいくつまである？",
+
+answers:[
+"Part.8",
+"Part.12",
+"Part.20",
+"Part.24"
+],
+
+correct:1
+},
+
+
+{
+question:"「チーム友達 Remix」で実在しないのはどれ？",
+
+answers:[
+"沖縄 Remix",
+"青森 Remix",
+"九州 Remix",
+"東海 Remix"
+],
+
+correct:0
+},
+
+
+{
+question:"AKLOの曲はどれ？",
+
+answers:[
+"GIOTF",
+"N.G.R",
+"RGTO",
+"PPAP"
+],
+
+correct:2
+},
+
+
+{
+question:"スチャダラパーの「あんた誰？」で共演した人は？",
+
+answers:[
+"いかりや長介",
+"坂上二郎",
+"ビートたけし",
+"谷啓"
+],
+
+correct:3
+},
+
+
+{
+question:"MASATO HAYASHIの曲はどれ？",
+
+answers:[
+"HIROAKI",
+"NORIKIYO",
+"HIROYUKI",
+"TAKAHIRO"
+],
+
+correct:2
+},
+
+
+{
+question:"Zeebraの「Street Dreams」の歌詞に出てこないグループは？",
+
+answers:[
+"雷",
+"Soul Scream",
+"Rhymester",
+"Buddha Brand"
+],
+
+correct:1
+},
+
+
+{
+question:"Lamp Eyeの「証言」で5番目にラップしているのは誰？",
+
+answers:[
+"Twigy",
+"Gama",
+"Zeebra",
+"Dev Large"
+],
+
+correct:0
+},
+
+
+{
+question:"スチャダラパーからのライムスターの「Forever Young」Remix ver.でコラボした芸人は誰？",
+
+answers:[
+"オードリー春日",
+"バナナマン日村",
+"くりいむしちゅー有田",
+"アンタッチャブル山崎"
+],
+
+correct:3
+},
+
+
+{
+question:"ZORNの歌詞に出てくる缶コーヒーの名前は？",
+
+answers:[
+"Emerald Mountain",
+"BOSS",
+"Max Coffee",
+"Wonda"
+],
+
+correct:0
+}
+
+];
---
+++ ushiphop.js
@@ -0,0 +1,155 @@
+let usQuiz = [
+{
+question:"Wu-Tang Clanのメンバーではないのは誰？",
+
+answers:[
+"RZA",
+"SZA",
+"GZA",
+"ODB"
+],
+
+correct:1
+},
+
+
+{
+question:"実在するラッパーは誰？",
+
+answers:[
+"REDMAN",
+"GREENMAN",
+"BLACKMAN",
+"BLUEMAN"
+],
+
+correct:0
+},
+
+
+{
+question:"LL Cool Jとは何の略？",
+
+answers:[
+"Little Love Cool John",
+"Ladies Love Cool James",
+"Long Love Cool Johnny",
+"Love Love Cool Jam"
+],
+
+correct:1
+},
+
+
+{
+question:"エミネムの出身地は？",
+
+answers:[
+"New Orleans",
+"Los Angeles",
+"Detroit",
+"Chicago"
+],
+
+correct:2
+},
+
+
+{
+question:"ウィル・スミスの旧芸名は？",
+
+answers:[
+"DJ Jazzy Jeff",
+"The Fresh Prince",
+"Prince Rakeem",
+"DJ MASTERKEY"
+],
+
+correct:1
+},
+
+
+{
+question:"NASのファーストアルバムのタイトルは？",
+
+answers:[
+"illmatic",
+"stillmatic",
+"cinematic",
+"automatic"
+],
+
+correct:0
+},
+
+
+{
+question:"カニエ・ウエストの娘はどれ？",
+
+answers:[
+"North・West",
+"East・West",
+"South・West",
+"West・West"
+],
+
+correct:0
+},
+
+
+{
+question:"実在するラッパーは誰？",
+
+answers:[
+"18savage",
+"19savage",
+"20savage",
+"21savage"
+],
+
+correct:3
+},
+
+
+{
+question:"09年グラストンベリーでJAY-Zが歌ったOASISの曲は何？",
+
+answers:[
+"Live Forever",
+"Whatever",
+"Wonderwall",
+"Stand By Me"
+],
+
+correct:2
+},
+
+
+{
+question:"2025年スーパーボウルハーフタイムショーに出演したスポーツ選手は誰？",
+
+answers:[
+"Usain Bolt",
+"Serena Williams",
+"Mookie Betts",
+"Alysa Liu"
+],
+
+correct:1
+},
+
+
+{
+question:"Wu-Tang Clanのメンバーなのは誰？",
+
+answers:[
+"A-God",
+"G-God",
+"I-God",
+"U-God"
+],
+
+correct:3
+}
+
+];
